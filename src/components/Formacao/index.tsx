a551d309be4dff8c1448f5245ec819c7430cc238
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-03-31 20:35:19 -0300

    syntax

--- src/components/Formacao/index.tsx
+++ src/components/Formacao/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import Carousel from 'react-elastic-carousel'
 import './Formacao.scss';
+import Carousel from 'react-elastic-carousel';
+import { idText } from 'typescript';
 
 interface Formacao 
 {
@@ -23,42 +24,40 @@ const formacoes: Formacao[] = [
 
 
 
-export default function FormacaoAcademica ()
+const FormacaoAcademica = () =>
 {
 
     const [ formacoesAcademicas, setFormacoesAcademicas ] = useState(formacoes);
     const [ currentIndex, setCurrentIndex ] = useState(0);
 
+
+
     return (
-        <div className="container">
-            <div className="controls-wrapper"></div>
-            <hr className='separator' />
-            <div className="carrousel-wrapper"></div>   
-        </div>
-    );
-    
-}
+        <div className="formacao-academica">
+            <h2 className="formacao-academica__titulo">Formação Acadêmica</h2>
+            <div className="formacao-academica__carrossel">
+                <Carousel key={formacoes}
+                    isRTL
+                    itemsToShow={3}
+                    
 
-    
-
-
-
-
-/* {formacoesAcademicas.map((formacao) => (
-        <div key={formacao.id} className="formacao">
-            <img src={formacao.logo} alt={formacao.empresa} />
-            <div className="curso">
-                <span className="empresa">{formacao.empresa}</span>
-                <hr />
-                <span className="nome-curso">{formacao.curso}</span>
-                <span className="data">{formacao.data}</span>
+                >
+                    {formacoesAcademicas.map((formacao) => (
+                        <div key={formacao.id} className="formacao">
+                            <img src={formacao.logo} alt={formacao.empresa} />
+                            <div className="curso">
+                                <span className="empresa">{formacao.empresa}</span>
+                                <hr />
+                                <span className="nome-curso">{formacao.curso}</span>
+                                <span className="data">{formacao.data}</span>
+                            </div>
+                        </div>
+                    ))}
+                </Carousel>
             </div>
         </div>
-    ))} 
-    
-    
-    
-    <div className="formacao-academica">
-            <h2 className="formacao-academica__titulo">Formação Acadêmica</h2>
-            <div className="formacao-academica__carrossel"></div>
-        </div>*/
+    );
+
+}
+
+export default FormacaoAcademica

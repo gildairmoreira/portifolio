9909a1891f05253cebe4ea05f7f1622552b93c65
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2025-03-11 11:32:51 -0300

    feat: adicionando integração com Vercel Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './pages/App';
 import './i18n/i18n'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Analytics />
     <SpeedInsights />
     <App />
   </React.StrictMode>
